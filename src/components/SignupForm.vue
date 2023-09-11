4224a65aa5aaf662e1bfe52e2f40d308a0d0c499
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-09-11 19:11:55 +0300

    Add 2-way data binding for inputs and select box

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,13 +1,31 @@
 <template>
   <form>
     <label>Email:</label>
-    <input type="email" required>
+    <input type="email" required v-model="email">
+
+    <label>Password:</label>
+    <input type="password" required v-model="password">
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
   </form>
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      email: '',
+      password: '',
+      role: 'designer'
+    }
+  }
 }
 </script>
 
@@ -29,7 +47,8 @@ export default {
     letter-spacing: 1px;
     font-weight: bold;
   }
-  input {
+  input,
+  select {
     display: block;
     padding: 10px 6px;
     width: 100%;
