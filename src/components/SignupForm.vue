5a75645537998774d12a99a5dbd3317b4695c067
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-09-12 16:44:43 +0300

    Add styles to skills, remove skills

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -22,7 +22,7 @@
     <label>Skills:</label>
     <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
     <div v-for="skill in skills" :key="skill" class="pill">
-      {{ skill }}
+      <span @click="removeSkill(skill)">{{ skill }}</span>
     </div>
 
     <!-- Multiple checkboxes -->
@@ -62,13 +62,17 @@ export default {
   },
   methods: {
     addSkill(e) {
-      console.log(e)
       if (e.key === ',' && this.tempSkill) {
         if (!this.skills.includes(this.tempSkill)) {
           this.skills.push(this.tempSkill)
         }
         this.tempSkill = ''
       }
+    },
+    removeSkill(skill) {
+      this.skills = this.skills.filter((item) => {
+        return item !== skill
+      })
     }
   }
 }
@@ -108,5 +112,17 @@ export default {
     margin: 0 10px 0 0;
     position: relative;
     top: 2px;
+  }
+  .pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
   }
 </style>
